6bdb0116bbd6997e23ec69bd531cc3b732617655
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-08-07 23:12:26 +0530

    Add files via upload

---
+++ apna college using do while.java
@@ -0,0 +1,16 @@
+public class Main{ 
+    public static void main(String[] args) {
+        int n=11;
+        int i=12;
+        while(i<11){
+            System.out.println("Apna College");
+            
+        }
+        do{
+            System.out.println("Apna College");
+        }while(i<11);
+       
+        
+    }
+    
+}
