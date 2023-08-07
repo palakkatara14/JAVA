35efff5aedf5edf6f986b88f6e158ec6cb638733
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-08-07 23:14:22 +0530

    Add files via upload

---
+++ java 1.java
@@ -0,0 +1,7 @@
+class variables{
+    public static void main(String [] args) {     //int main()
+        int a=10;
+        //printf("%d",a);
+        System.out.println(a);
+    }
+}
---
+++ using do while in java.java
@@ -0,0 +1,14 @@
+public class Main{ 
+    public static void main(String[] args) {
+        int n=11;
+        int i=0;
+        do{
+            System.out.println(i);
+            i=i+1; 
+        }
+        while(i<n);
+       
+        
+    }
+    
+}
