bacb61337585b6a0dc6910758b9ddb6d9113f1ac
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-08-17 09:27:22 +0530

    Add files via upload

---
+++ convert celsius to fahrehneit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int c=sc.nextInt();
+        float f=c*(9/5)+32;
+        System.out.println(f);
+    }
+}
---
+++ square root.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){  
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+               
+        
+        
+        System.out.println(Math.sqrt(a));
+        
+    }
+} 
