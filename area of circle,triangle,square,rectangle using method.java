fc65daad2c186bbac151667664ed8c364a2ae577
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-08-23 20:06:31 +0530

    Update area of circle,triangle,square,rectangle using method.java

--- area of circle,triangle,square,rectangle using method.java
+++ area of circle,triangle,square,rectangle using method.java
@@ -1,34 +1,40 @@
+import java.util.*;
 class Area{
-private double height;
-private double breadth;
-private double length;
-private double radius;
-void setHeight(double d){height=d;}
-void setBreadth(double d){breadth=d;}
-void setLength(double d){length=d;}
-void setRadius(double d){radius=d;}
-double circle(){return 3.14*radius*radius;}
-double square(){return length*breadth;}
-double rectangle(){return length*breadth;}
-double triangle(){return 0.5*breadth*height;}
+    private double height;
+    private double breadth;
+    private double length;
+    private double radius;
+    void setHeight(double d){height=d;}
+    void setBreadth(double d){breadth=d;}
+    void setLength(double d){length=d;}
+    void setRadius(double d){radius=d;}
+    double circle(){return 3.14*radius*radius;}
+    double square(){return length*breadth;}
+    double rectangle(){return length*breadth;}
+    double triangle(){return 0.5*breadth*height;}
 
 }
 
 public class Main{
-public static void main(String[]args){
-Area a=new Area();
-a.setHeight(2);
-a.setBreadth(3);
-a.setLength(4);
-a.setRadius(5);
+      public static void main(String[]args){
+           Scanner sc=new Scanner(System.in);
+           double height=sc.nextDouble();
+           double breadth=sc.nextDouble();
+           double length=sc.nextDouble();
+           double radius=sc.nextDouble();
+           Area a=new Area();
+           a.setHeight(height);
+           a.setBreadth(breadth);
+           a.setLength(length);
+           a.setRadius(radius);
 
-double circle=a.circle();
-System.out.println(circle);
-double square=a.square();
-System.out.println(square);
-double rectangle=a.rectangle();
-System.out.println(rectangle);
-double triangle=a.triangle();
-System.out.println(triangle);
+          double circle=a.circle();
+          System.out.println(circle);
+          double square=a.square();
+          System.out.println(square);
+          double rectangle=a.rectangle();
+          System.out.println(rectangle);
+         double triangle=a.triangle();
+         System.out.println(triangle);
 }
 }
