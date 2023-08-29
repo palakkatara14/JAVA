0d7bccf21bf6c1a9302e5e9d85701cef22cfbe1e
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-08-29 23:50:19 +0530

    Add files via upload

---
+++ number is positive ,negative,zero using if else.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        
+        if(a>0){
+            System.out.println("positive");
+        }
+        else if(a<0){
+            System.out.println("negative");
+        }
+        else if(a==0){
+            System.out.println("zero");
+        }
+        else{
+            System.out.println("invalid output");
+        }
+    }
+}
