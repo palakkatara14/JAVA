67b80567ba2a28d5e91557f45a89366eb21a9c82
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-09-09 16:03:42 +0530

    Update table using loop.java

--- table using loop.java
+++ table using loop.java
@@ -1,13 +1,1 @@
-import java.util.*;
-public class Main{ 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        
-        for(int i=1;i<11;i++) {
-        System.out.println(n*i);
-        }
-        
-    }
-    
-}
+
