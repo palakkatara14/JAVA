fb734f2a29656e86cc5054567556f064a7b31fc8
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-09-13 02:15:56 +0530

    Update linear search.java

--- linear search.java
+++ linear search.java
@@ -1,25 +1,19 @@
 //WAP to check if a given number exist in the array or not(LINEAR SEARCH).
 import java.util.*;
 public class Main{
-    
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int numbers[]=new int[size];
-        System.out.println("enter the values");
-        for(int i=0;i<size;i++){
-            numbers[i]=sc.nextInt();
-         }
-        System.out.print("the created array is:");
-        for(int i=0;i<size;i++){
-            System.out.print(numbers[i]+" ");
+   
+    public  boolean Search_Element(int[] arr,int n){
+        
+       System.out.print("the created array is:");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
         System.out.print("enter a number to be found:");
         int x=sc.nextInt();
         boolean exist=false;
-        for(int i=0;i<size;i++){
-            if(numbers[i]==x){
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
                 exist=true;
                 break;
             }
@@ -32,12 +26,27 @@ public class Main{
             System.out.println("not exist");
         }
         
-            
-        }
-           
         
+        
+        
+   }
+   public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+      
+        int[] arr=new int[n];
        
-    }
-
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        
+        
+         Search_Element(arr,n);
+   }
+   
+   
+   
+}
 
 
