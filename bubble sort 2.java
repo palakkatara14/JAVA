cea2d6106868555bece5a4003587fc8a7b35cf50
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-09-13 02:13:19 +0530

    Update bubble sort 2.java

--- bubble sort 2.java
+++ bubble sort 2.java
@@ -1,22 +1,9 @@
 import java.util.*;
-
 public class Main{
    
-   public static void main(String[]args){
-   
-     
-     Scanner sc=new Scanner(System.in);
-     System.out.println("enter the size/length of array");
-     int n=sc.nextInt();
-     int[] arr=new int[n];
-    
-     
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-            
-           
-            
-        }
+    public static void Array_Sort(int[] arr,int n){
+        
+       
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
@@ -33,14 +20,28 @@ public class Main{
         }
         
         
+        
+   }
+   public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+      
+        int[] arr=new int[n];
        
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         
         
         
-        
+         Array_Sort(arr,n);
    }
    
    
    
 }
-  
+        
+       
+        
+        
+        
