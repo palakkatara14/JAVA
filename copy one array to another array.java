2f3efbcd7de4afa702497a1a1e93948410bfc490
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-09-13 02:14:39 +0530

    Update copy one array to another array.java

--- copy one array to another array.java
+++ copy one array to another array.java
@@ -1,18 +1,11 @@
 import java.util.*;
-
 public class Main{
    
-   public static void main(String[]args){
-   
-     
-     Scanner sc=new Scanner(System.in);
-     System.out.println("enter the size/length of array");
-     int n=sc.nextInt();
-     int[] arr1=new int[n];
-    
-     
-        for(int i=0;i<n;i++){
-            arr1[i]=sc.nextInt();
+    public static void Array_Copy(int[] arr1,int n){
+        
+       System.out.print("the created array is:");
+         for(int i=0;i<n;i++){
+            System.out.println(arr1[i]);
             
            
             
@@ -32,10 +25,22 @@ public class Main{
         
         
         
+   }
+   public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+      
+        int[] arr1=new int[n];
+       
+        for(int i=0;i<n;i++){
+            arr1[i]=sc.nextInt();
+        }
         
+        
+        
+         Array_Copy(arr1,n);
    }
    
    
    
 }
-  
