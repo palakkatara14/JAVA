e0c0ba18dc30204eb36f2174858e9c07c621318d
palak katara <122154471+palakkatara14@users.noreply.github.com>
2023-09-28 01:18:09 +0530

    Add files via upload

---
+++ array leader.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{   public static void leader(int[] a,int n)
+    {
+        for (int i = 0; i <n; i++) 
+        {
+            int j;
+            for (j = i + 1; j < n; j++) 
+            {
+                if (a[i] <= a[j])
+                    break;
+            }
+            if (j == n) 
+                System.out.print(a[i] + " ");
+
+        }
+        
+    
+        
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0; i<n; i++){
+	     	a[i]=sc.nextInt();
+		}
+		leader(a,n);
+		
+	}
+}
---
+++ combinations.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main{
+
+     
+        public static int factorial(int n){
+                int f=1;
+                
+                for(int i=1;i<=n;i++){
+                    f=f*i;
+                    
+                }
+                return f;
+                
+                
+        }
+		          
+		public static void main(String[]args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter the number:");
+            int n=sc.nextInt();
+            int r=sc.nextInt();
+            int a=factorial(n);
+            int b=factorial(r)*factorial(n-r);
+            System.out.println(a/b);
+        
+    
+    }
+    
+}
